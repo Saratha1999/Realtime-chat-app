d2a15e9cf4c5b6c04c4d7f3380f691410986ee5f
Saratha1999 <ldsarathadevi1999@gmail.com'cleargit config --global user.email ldsarathadevi1999@gmail.com>
2025-05-26 21:23:31 -0500

    Fix test with user login to avoid 302 redirect

--- chat/tests.py
+++ chat/tests.py
@@ -1,7 +1,12 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 class ChatRoomViewTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.client.login(username='testuser', password='testpass')
+
     def test_room_view_renders(self):
         room_name = "testroom"
         response = self.client.get(reverse("room", args=[room_name]))
