bcef10f8fd0a0bcc8ed05eb22f8728178ac96559
Saratha1999 <ldsarathadevi1999@gmail.com'cleargit config --global user.email ldsarathadevi1999@gmail.com>
2025-05-26 21:30:16 -0500

    Fix 404 by updating chat room test URL

--- chat/tests.py
+++ chat/tests.py
@@ -1,17 +1,12 @@
 from django.test import TestCase
-from django.urls import reverse
 from django.contrib.auth.models import User
 
 class ChatRoomViewTest(TestCase):
     def setUp(self):
-        # Create test user
         self.user = User.objects.create_user(username='testuser', password='testpass')
-        # Log in test user
         self.client.login(username='testuser', password='testpass')
 
     def test_room_view_renders(self):
-        room_name = "testroom"
-        url = reverse("room", args=[room_name])
-        response = self.client.get(url)
+        response = self.client.get("/chat/testroom/")
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, room_name)
+        self.assertContains(response, "testroom")
