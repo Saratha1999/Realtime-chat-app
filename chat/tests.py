eb036cc66237b7598499f62f37c181800877d63f
Saratha1999 <ldsarathadevi1999@gmail.com'cleargit config --global user.email ldsarathadevi1999@gmail.com>
2025-05-26 21:26:12 -0500

    Fix 302 error by logging in test user

--- chat/tests.py
+++ chat/tests.py
@@ -4,11 +4,14 @@ from django.contrib.auth.models import User
 
 class ChatRoomViewTest(TestCase):
     def setUp(self):
+        # Create test user
         self.user = User.objects.create_user(username='testuser', password='testpass')
+        # Log in test user
         self.client.login(username='testuser', password='testpass')
 
     def test_room_view_renders(self):
         room_name = "testroom"
-        response = self.client.get(reverse("room", args=[room_name]))
+        url = reverse("room", args=[room_name])
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, room_name)
